d7d557f49b22d58e146d3f975866cf531e7b09fc
hlcheuk <honlun.cheuk@gmail.com>
2024-10-14 01:21:11 +0800

    introduce azure gpt llms

--- src/model_init/llms.py
+++ src/model_init/llms.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_core.runnables import ConfigurableField
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, AzureChatOpenAI
 
 from src.utils.utils import read_yaml
 
@@ -12,9 +12,15 @@ load_dotenv(override=True)
 llms_config = read_yaml("config/setting.yaml")["llms"]
 
 # Get model config for Github models
-github_config = llms_config["github"]
-gpt_4o_mini_config = github_config["gpt-4o-mini"]
-gpt_4o_config = github_config["gpt-4o"]
+# github_config = llms_config["github"]
+# gpt_4o_mini_config = github_config["gpt-4o-mini"]
+# gpt_4o_config = github_config["gpt-4o"]
+
+# Get model config for Azure models
+azure_config = llms_config["azure"]
+gpt_4o_mini_config = azure_config["gpt-4o-mini"]
+gpt_4o_config = azure_config["gpt-4o"]
+
 
 # Get model config for yi-01 models
 yi01_config = llms_config["yi-01"]
@@ -22,11 +28,50 @@ yi_large_config = yi01_config["yi-large"]
 yi_large_turbo_config = yi01_config["yi-large-turbo"]
 yi_spark_config = yi01_config["yi-spark"]
 
-llm_gpt_4o_mini = ChatOpenAI(
+# llm_gpt_4o_mini = ChatOpenAI(
+#     model=gpt_4o_mini_config["model_name"],
+#     openai_api_base=os.getenv("GITHUB_ENDPOINT"),
+#     openai_api_key=os.getenv("GITHUB_TOKEN"),
+#     max_tokens=gpt_4o_mini_config["max_tokens"],
+# ).configurable_fields(
+#     temperature=ConfigurableField(
+#         id="temperature",
+#         name="LLM Temperature",
+#         description="The temperature of the LLM",
+#     ),
+#     top_p=ConfigurableField(
+#         id="top_p",
+#         name="Top P of LLM",
+#         description="The top p of the LLM",
+#     ),
+# )
+
+# llm_gpt_4o = ChatOpenAI(
+#     model=gpt_4o_config["model_name"],
+#     openai_api_base=os.getenv("GITHUB_ENDPOINT"),
+#     openai_api_key=os.getenv("GITHUB_TOKEN"),
+#     max_tokens=gpt_4o_config["max_tokens"],
+# ).configurable_fields(
+#     temperature=ConfigurableField(
+#         id="temperature",
+#         name="LLM Temperature",
+#         description="The temperature of the LLM",
+#     ),
+#     top_p=ConfigurableField(
+#         id="top_p",
+#         name="Top P of LLM",
+#         description="The top p of the LLM",
+#     ),
+# )
+
+llm_gpt_4o_mini = AzureChatOpenAI(
     model=gpt_4o_mini_config["model_name"],
-    openai_api_base=os.getenv("GITHUB_ENDPOINT"),
-    openai_api_key=os.getenv("GITHUB_TOKEN"),
     max_tokens=gpt_4o_mini_config["max_tokens"],
+    azure_deployment=gpt_4o_mini_config["deployment_name"],
+    azure_endpoint=gpt_4o_mini_config["azure_openai_endpoint"],
+    openai_api_type=gpt_4o_mini_config["azure_openai_api_type"],
+    openai_api_version=gpt_4o_mini_config["azure_openai_api_version"],
+    openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
 ).configurable_fields(
     temperature=ConfigurableField(
         id="temperature",
@@ -40,11 +85,14 @@ llm_gpt_4o_mini = ChatOpenAI(
     ),
 )
 
-llm_gpt_4o = ChatOpenAI(
+llm_gpt_4o = AzureChatOpenAI(
     model=gpt_4o_config["model_name"],
-    openai_api_base=os.getenv("GITHUB_ENDPOINT"),
-    openai_api_key=os.getenv("GITHUB_TOKEN"),
     max_tokens=gpt_4o_config["max_tokens"],
+    azure_deployment=gpt_4o_config["deployment_name"],
+    azure_endpoint=gpt_4o_config["azure_openai_endpoint"],
+    openai_api_type=gpt_4o_config["azure_openai_api_type"],
+    openai_api_version=gpt_4o_config["azure_openai_api_version"],
+    openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
 ).configurable_fields(
     temperature=ConfigurableField(
         id="temperature",
